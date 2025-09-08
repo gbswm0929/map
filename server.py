2d93bd172266ad3e0ec645dc69968703138b271d
gbswm0929 <gbswst0929@gmail.com>
2025-09-08 20:12:47 +0900

    commit

---
+++ server.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request
+import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def get_index():
+    return render_template("main.html")
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=8080)
