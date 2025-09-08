e953d7be7369e6f2ef267c9c5fa9ef66c1b2a737
gbswm0929 <gbswst0929@gmail.com>
2025-09-08 21:52:11 +0900

    commit

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import datetime
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def get_index():
-    return 'asdf'
+    return render_template("main.html")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run()
