d438ff7be77ef4d36185a7a40f30a37247326d26
gbswm0929 <gbswst0929@gmail.com>
2025-09-10 16:28:17 +0900

    commit

--- server.py
+++ server.py
@@ -1,11 +1,28 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Response
+import requests
+from dotenv import load_dotenv
 import os
 
 app = Flask(__name__)
+load_dotenv()
 
 @app.route("/")
 def get_index():
-    return render_template("main.html", key = os.getenv("key"))
+    # return render_template("main.html")
+    return render_template("main copy.html")
+@app.route("/apis/map")
+def apimap():
+    try:
+        response = requests.get(f"https://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey={os.getenv("apismap")}&libraries=services")
+        response.raise_for_status()
+        if response.ok:
+            return Response(response.text, mimetype="application/javascripts")
+        else:
+            return
+    except requests.exceptions.RequestException as e:
+        print("/apis/map error : ", e)
+        return
+        
 
 if __name__ == "__main__":
     app.run()
