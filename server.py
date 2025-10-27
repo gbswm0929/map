925f7a99ea871296cb6ce0e450a26385d179646a
gbswm0929 <gbswst0929@gmail.com>
2025-10-27 13:21:04 +0900

    commit

--- server.py
+++ server.py
@@ -9,8 +9,8 @@ load_dotenv()
 def get_index():
     return render_template("main.html", key=os.getenv("key"))
 @app.route("/hi")
-def get_index():
-    return "hi"
+def get_hi():
+    return "hi, hello"
 
 if __name__ == "__main__":
     app.run()
